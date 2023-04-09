2de9ab497c3f2eb1baeb04788d6401e44b336715
jimmy <toluadejimi@gmail.com>
2023-04-09 06:55:17 +0100

    auth email

--- app/Http/Controllers/Auth/RegisterationController.php
+++ app/Http/Controllers/Auth/RegisterationController.php
@@ -320,6 +320,275 @@ class RegisterationController extends Controller
 
     }
 
+    public function auth_email_verification(Request $request)
+    {
+
+        try {
+
+
+            $user_id = Auth::id();
+
+            $email = $request->email;
+
+            $sms_code = random_int(1000, 9999);
+
+            $check_status = User::where('email', $email)->first()->status ?? null;
+            $check_email = User::where('email', $email)->first()->email ?? null;
+            $check_email_verification = User::where('email', $email)->first()->is_email_verified ?? null;
+
+            if ($check_email == $email && $check_status == 3) {
+
+                return response()->json([
+                    'status' => $this->failed,
+                    'message' => 'Account has been Restricted on ENKPAY',
+                ], 500);
+
+            }
+
+            if ($check_email == $email && $check_email_verification == 1) {
+
+                return response()->json([
+                    'status' => $this->failed,
+                    'message' => 'Email Already Exist',
+                ], 500);
+
+            }
+
+            if ($check_email == null && $check_email_verification == null && $user_id == null) {
+
+                $update_code = User::where('id', $user_id)
+                    ->update([
+                        'email' => $email,
+                    ]);
+
+
+                $data = array(
+                    'fromsender' => 'noreply@enkpayapp.enkwave.com', 'EnkPay',
+                    'subject' => "One Time Password",
+                    'toreceiver' => $email,
+                    'sms_code' => $sms_code,
+                );
+
+                Mail::send('emails.registration.otpcode', ["data1" => $data], function ($message) use ($data) {
+                    $message->from($data['fromsender']);
+                    $message->to($data['toreceiver']);
+                    $message->subject($data['subject']);
+                });
+
+                return response()->json([
+                    'status' => $this->success,
+                    'message' => "OTP Code has been sent succesfully to $email",
+                ], 200);
+
+            }
+
+            if ($check_email == $email && $check_email_verification == 0) {
+
+                $update_code = User::where('id', $user_id)
+                    ->update([
+                        'sms_code' => $sms_code,
+                    ]);
+
+                $data = array(
+                    'fromsender' => 'noreply@enkpayapp.enkwave.com', 'EnkPay',
+                    'subject' => "One Time Password",
+                    'toreceiver' => $email,
+                    'sms_code' => $sms_code,
+                );
+
+                Mail::send('emails.registration.otpcode', ["data1" => $data], function ($message) use ($data) {
+                    $message->from($data['fromsender']);
+                    $message->to($data['toreceiver']);
+                    $message->subject($data['subject']);
+                });
+
+                return response()->json([
+                    'status' => $this->success,
+                    'message' => 'OTP Code has been sent succesfully',
+                ], 200);
+
+            }
+
+        } catch (\Exception$e) {
+            return $e->getMessage();
+        }
+
+    }
+
+    public function auth_phone_verification(Request $request)
+    {
+
+        try {
+
+
+
+            $get_phone_no = $request->phone_no;
+            $trimed_no = substr($get_phone_no, 1);
+
+            $user_id = Auth::id() ?? null;
+
+
+
+
+            if (str_contains($get_phone_no, '+234')) {
+                $phone_no = $get_phone_no;
+            }else{
+                $phone_no = "+234".$trimed_no;
+            }
+
+
+            $sms_code = random_int(1000, 9999);
+
+            $check_phone_verification = User::where('phone', $phone_no)->first()->is_phone_verified ?? null;
+            $check_phone = User::where('phone', $phone_no)->first()->phone ?? null;
+            $check_status = User::where('phone', $phone_no)->first()->status ?? null;
+
+            if ($check_phone == $phone_no && $check_status == 3) {
+
+                return response()->json([
+                    'status' => $this->failed,
+                    'message' => 'Phone number has been Restricted on ENKPAY',
+                ], 500);
+
+            }
+
+            if ($check_phone == $phone_no && $check_phone_verification == 1) {
+
+
+                return response()->json([
+                    'status' => $this->failed,
+                    'message' => 'Phone Number Already Exist',
+                ], 500);
+
+            }
+
+
+
+
+
+            if ($check_phone == null && $check_phone_verification == null && $user_id == null) {
+
+                $update_code = User::where('id', $user_id)
+                    ->update([
+                        'phone' => $phone,
+                    ]);
+
+                $token = $user->createToken('API Token')->accessToken;
+
+                $headers = [
+                    'Accept' => 'application/json',
+                    'Content-Type' => 'application/json',
+                    'Authorization' => 'Bearer FLWSECK-043cf4e9dd848683c6b157c234ba2fb8-X',
+                ];
+
+                $client = new GuzzleClient([
+                    'headers' => $headers,
+                ]);
+
+                $response = $client->request('POST', 'https://api.ng.termii.com/api/sms/send',
+                    [
+                        'body' => json_encode([
+                            "api_key" => "TLxF6Jauos8AJq6pKztkbxaQJbQjZzs43vJLOsXk8fHcUez3mBolehZGGzTwnF",
+                            "to" => $phone_no,
+                            "from" => "N-Alert",
+                            "sms" => "Your Enkwave confirmation code is $sms_code. active for 5 minutes, one-time use only",
+                            "type" => "plain",
+                            "channel" => "dnd",
+
+                        ]),
+
+                    ]);
+
+                $body = $response->getBody();
+                $result = json_decode($body);
+
+                return response()->json([
+                    'status' => $this->success,
+                    'message' => 'OTP Code has been sent succesfully',
+                ], 200);
+
+            }
+
+            if ($check_phone == $phone_no && $check_phone_verification == 0) {
+
+                $update_code = User::where('id', $user_id)
+                    ->update([
+                        'sms_code' => $sms_code,
+                    ]);
+
+                $curl = curl_init();
+                $data = array(
+
+                    "api_key" => "TLxF6Jauos8AJq6pKztkbxaQJbQjZzs43vJLOsXk8fHcUez3mBolehZGGzTwnF",
+                    "to" => $phone_no,
+                    "from" => "N-Alert",
+                    "sms" => "Your Verification Code is $sms_code",
+                    "type" => "plain",
+                    "channel" => "generic",
+
+                );
+
+                $post_data = json_encode($data);
+
+                curl_setopt_array($curl, array(
+                    CURLOPT_URL => "https://api.ng.termii.com/api/sms/send",
+                    CURLOPT_RETURNTRANSFER => true,
+                    CURLOPT_ENCODING => "",
+                    CURLOPT_MAXREDIRS => 10,
+                    CURLOPT_TIMEOUT => 0,
+                    CURLOPT_FOLLOWLOCATION => true,
+                    CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+                    CURLOPT_CUSTOMREQUEST => "POST",
+                    CURLOPT_POSTFIELDS => $post_data,
+                    CURLOPT_HTTPHEADER => array(
+                        "Content-Type: application/json",
+                    ),
+                ));
+
+                $var = curl_exec($curl);
+                curl_close($curl);
+
+                $var = json_decode($var);
+
+                $status = $var->message;
+
+                if ($status == 'Successfully Sent') {
+
+                    return response()->json([
+                        'status' => $this->success,
+                        'message' => 'OTP Code has been sent succesfully',
+                    ], 200);
+
+                }
+
+                return response()->json([
+                    'status' => $this->failed,
+                    'message' => 'Service not available at the moment, Please try using Email',
+                ], 500);
+
+            }
+
+        } catch (\Exception$e) {
+            return $e->getMessage();
+        }
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     public function resend_email_otp(Request $request)
     {
 
