b564d298f98ad6c5d1dbe504ba3f85227542f94c
jimmy <toluadejimi@gmail.com>
2023-04-29 08:44:31 +0100

    push

--- app/Http/Controllers/Virtual/VirtualaccountController.php
+++ app/Http/Controllers/Virtual/VirtualaccountController.php
@@ -7,12 +7,10 @@ use App\Models\Charge;
 use App\Models\Transaction;
 use App\Models\User;
 use Auth;
+use DateTimeZone;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
 use Mail;
-use DateTime;
-use DateTimeZone;
-
 
 class VirtualaccountController extends Controller
 {
@@ -45,7 +43,7 @@ class VirtualaccountController extends Controller
 
                 return response()->json([
                     'status' => $this->failed,
-                    'message' => 'User has neem restricted on ENKPAY',
+                    'message' => 'User has been restricted on ENKPAY',
                 ], 500);
 
             }
@@ -140,9 +138,61 @@ class VirtualaccountController extends Controller
                 $user->v_account_no = $acct_no;
                 $user->v_account_name = $acct_name;
                 $user->save();
+            } else {
+
+                return response()->json([
+
+                    'status' => $this->failed,
+                    'message' => $var->error->message,
+
+                ], 500);
+
+            }
+
+            $curl = curl_init();
+            $data = array(
+                "account_name" => $name,
+                "bvn" => $bvn,
+            );
+
+            $databody = json_encode($data);
+            curl_setopt_array($curl, array(
+                CURLOPT_URL => 'http://154.113.16.142:8088/appdevapi/api/PiPCreateReservedAccountNumber',
+                CURLOPT_RETURNTRANSFER => true,
+                CURLOPT_ENCODING => '',
+                CURLOPT_MAXREDIRS => 10,
+                CURLOPT_TIMEOUT => 0,
+                CURLOPT_FOLLOWLOCATION => true,
+                CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+                CURLOPT_CUSTOMREQUEST => 'POST',
+                CURLOPT_POSTFIELDS => $databody,
+                CURLOPT_HTTPHEADER => array(
+                    'Content-Type: application/json',
+                    'Accept: application/json',
+                    'Client-Id: dGVzdF9Qcm92aWR1cw==',
+                    'X-Auth-Signature: BE09BEE831CF262226B426E39BD1092AF84DC63076D4174FAC78A2261F9A3D6E59744983B8326B69CDF2963FE314DFC89635CFA37A40596508DD6EAAB09402C7',
+                ),
+            ));
+
+            $var = curl_exec($curl);
+
+            curl_close($curl);
+            $var = json_decode($var);
+
+            $status = $var->responseCode ?? null;
+            $p_acct_no = $var->account_number ?? null;
+            $p_acct_name = $var->account_name ?? null;
+
+            $bank = "PROVIDUS BANK";
+
+            if ($status == 00) {
+
+                $user = User::find(Auth::id());
+                $user->p_account_no = $p_acct_no;
+                $user->p_account_name = $p_acct_name;
+                $user->save();
 
                 $get_user = User::find(Auth::id())->first();
-
                 return response()->json([
 
                     'status' => $this->success,
@@ -151,14 +201,16 @@ class VirtualaccountController extends Controller
 
                 ], 200);
 
+            } else {
+
+                return response()->json([
+
+                    'status' => $this->failed,
+                    'message' => 'Error please try again after some time',
+
+                ], 500);
+
             }
-
-            return response()->json([
-
-                'status' => $this->failed,
-                'message' => $var->error->message,
-
-            ], 500);
 
         } catch (\Exception $th) {
             return $th->getMessage();
@@ -339,19 +391,15 @@ class VirtualaccountController extends Controller
 
                         $curl = curl_init();
 
-                        $datetime = new \DateTime( "now", new DateTimeZone( "Europe/Bucharest" ) );
+                        $datetime = new \DateTime ("now", new DateTimeZone("Europe/Bucharest"));
 
                         $date1 = $datetime->format('Y-m-d');
                         $date2 = $datetime->format('H:i:s');
 
-
-
-
-
                         $data = array(
 
                             "Amount" => $Amount,
-                            "DateOfTransaction" => $date1."T".$date2."+"."01:00",
+                            "DateOfTransaction" => $date1 . "T" . $date2 . "+" . "01:00",
                             "SenderAccountNumber" => $sender_account_no,
                             "SenderAccountName" => $sender_name,
                             "OriginatorBank" => $sender_bank,
@@ -361,7 +409,6 @@ class VirtualaccountController extends Controller
                         );
 
                         $post_data = json_encode($data);
-
 
                         curl_setopt_array($curl, array(
                             CURLOPT_URL => 'https://api.errandpay.com/epagentservice/api/v1/Webhook/Notify',
@@ -496,6 +543,210 @@ class VirtualaccountController extends Controller
             $var = json_decode($var);
 
             dd($var, $errand_key, $acct_no);
+
+        } catch (\Exception $th) {
+            return $th->getMessage();
+        }
+
+    }
+
+    //PROVIDUS VIRTUAL ACCOUNT
+
+    public function providusCashIn(request $request)
+    {
+
+        try {
+
+            $header = $request->header('X-Auth-Signature');
+
+            $sessionId = $request->sessionId;
+            $accountNumber = $request->accountNumber;
+            $tranRemarks = $request->tranRemarks;
+            $settledAmount = $request->settledAmount;
+            $transactionAmount = $request->transactionAmount;
+            $feeAmount = $request->feeAmount;
+            $TransactionTime = $request->TransactionTime;
+            $initiationTranRef = $request->initiationTranRef;
+            $settlementId = $request->settlementId;
+            $sourceAccountNumber = $request->sourceAccountNumber;
+            $PostingType = $request->PostingType;
+            $TransactionReference = $request->TransactionReference;
+            $sourceAccountName = $request->sourceAccountName;
+            $sourceBankName = $request->sourceBankName;
+            $channelId = $request->channelId;
+            $tranDateTime = $request->tranDateTime;
+
+            $key = env('POKEY');
+
+            $deposit_charges = Charge::where('id', 2)->first()->amount;
+
+            $trans_id = "ENK-" . random_int(100000, 999999);
+
+            $verify1 = hash('sha512', $key);
+
+            $verify2 = strtoupper($verify1);
+
+
+            if ($verify2 == $header) {
+
+                $deposit_charges = Charge::where('id', 2)->first()->amount;
+
+                $main_wallet = User::where('p_account_no', $accountNumber)
+                    ->first()->main_wallet ?? null;
+
+                $user_id = User::where('p_account_no', $accountNumber)
+                    ->first()->id ?? null;
+
+                $user_email = User::where('p_account_no', $accountNumber)
+                    ->first()->email ?? null;
+
+                $first_name = User::where('p_account_no', $accountNumber)
+                    ->first()->first_name ?? null;
+
+                $last_name = User::where('p_account_no', $accountNumber)
+                    ->first()->last_name ?? null;
+
+                $check_status = User::where('p_account_no', $accountNumber)->first()->status ?? null;
+
+                $VirtualCustomerAccount = User::where('p_account_no', $accountNumber)->first()->v_account_no ?? null;
+
+                if ($main_wallet == null && $user_id == null) {
+
+                    return response()->json([
+                        'status' => false,
+                        'message' => 'V Account not registred on Enkpay',
+                    ], 500);
+
+                }
+
+                if ($check_status == 3) {
+
+                    return response()->json([
+                        'status' => $this->failed,
+                        'message' => 'Account has been Restricted on ENKPAY',
+                    ], 500);
+
+                }
+
+                // $enkpay_profit = $deposit_charges - 10;
+
+                // $message_amount = $Amount - 10;
+
+                //credit
+                // $enkpay_debit = $Amount - $deposit_charges;
+                $updated_amount = $main_wallet + $settledAmount;
+                $main_wallet = User::where('p_account_no', $accountNumber)
+                    ->update([
+                        'main_wallet' => $updated_amount,
+                    ]);
+
+                //update Transactions
+                $trasnaction = new Transaction();
+                $trasnaction->user_id = $user_id;
+                $trasnaction->ref_trans_id = $trans_id;
+                $trasnaction->e_ref = $sessionId;
+                $trasnaction->type = "PROVIDUS FUNDING";
+                $trasnaction->transaction_type = "VirtualFundWallet";
+                $trasnaction->title = "Wallet Funding";
+                $trasnaction->main_type = "Transfer";
+                $trasnaction->credit = $settledAmount;
+                $trasnaction->note = "$sourceAccountName | Wallet Funding";
+                $trasnaction->fee = $feeAmount;
+                $trasnaction->amount = $transactionAmount;
+                $trasnaction->e_charges = $deposit_charges;
+                $trasnaction->enkPay_Cashout_profit = 0;
+                $trasnaction->trx_date = $tranDateTime;
+                $trasnaction->trx_time = $tranDateTime;
+                $trasnaction->sender_name = $sourceAccountName;
+                $trasnaction->sender_bank = $sourceBankName;
+                $trasnaction->sender_account_no = $sourceAccountNumber;
+                $trasnaction->balance = $updated_amount;
+                $trasnaction->status = 1;
+                $trasnaction->save();
+
+                $errand_key = errand_api_key();
+
+                $b_code = env('BCODE');
+
+                $acct_no = $request->acct_no;
+
+                $curl = curl_init();
+
+                $datetime = new \DateTime ("now", new DateTimeZone("Europe/Bucharest"));
+
+                $date1 = $datetime->format('Y-m-d');
+                $date2 = $datetime->format('H:i:s');
+
+                $data = array(
+
+                    "Amount" => $transactionAmount,
+                    "DateOfTransaction" => $date1 . "T" . $date2 . "+" . "01:00",
+                    "SenderAccountNumber" => $sourceAccountNumber,
+                    "SenderAccountName" => $sourceAccountName,
+                    "OriginatorBank" => $sourceBankName,
+                    "RecipientAccountNumber" => $VirtualCustomerAccount,
+                    "RecipientAccountName" => $first_name . " " . $last_name,
+
+                );
+
+                $post_data = json_encode($data);
+
+                curl_setopt_array($curl, array(
+                    CURLOPT_URL => 'https://api.errandpay.com/epagentservice/api/v1/Webhook/Notify',
+                    CURLOPT_RETURNTRANSFER => true,
+                    CURLOPT_ENCODING => '',
+                    CURLOPT_MAXREDIRS => 10,
+                    CURLOPT_TIMEOUT => 0,
+                    CURLOPT_FOLLOWLOCATION => true,
+                    CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+                    CURLOPT_CUSTOMREQUEST => 'POST',
+                    CURLOPT_POSTFIELDS => $post_data,
+                    CURLOPT_HTTPHEADER => array(
+                        'epKey: ep_live_jFrIZdxqSzAdraLqbvhUfVYs',
+                        'Content-Type: application/json',
+                    ),
+                ));
+
+                $var = curl_exec($curl);
+                curl_close($curl);
+                $var = json_decode($var);
+
+                dd($var, $post_data);
+
+                $message = "Your Pool account has been credited |  $transactionAmount | from Virtual account";
+
+                send_notification($message);
+
+                //send to user
+
+                if ($user_email !== null) {
+
+                    $data = array(
+                        'fromsender' => 'noreply@enkpayapp.enkwave.com', 'EnkPay',
+                        'subject' => "Virtual Account Credited",
+                        'toreceiver' => $user_email,
+                        'amount' => $transactionAmount,
+                        'first_name' => $first_name,
+                    );
+
+                    Mail::send('emails.transaction.virtual-credit', ["data1" => $data], function ($message) use ($data) {
+                        $message->from($data['fromsender']);
+                        $message->to($data['toreceiver']);
+                        $message->subject($data['subject']);
+                    });
+                }
+
+                return response()->json([
+                    'status' => true,
+                    'message' => 'Tranasaction Successsfull',
+                ], 200);
+
+            }
+
+            return response()->json([
+                'status' => false,
+                'message' => 'Key not Authorized',
+            ], 500);
 
         } catch (\Exception $th) {
             return $th->getMessage();
