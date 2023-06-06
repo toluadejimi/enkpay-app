d5fd40e35150090f8f99fd5ea0c3a7549d9595b4
Jimmy <toluadejimi@gmail.com>
2023-06-06 18:06:28 +0100

    iiirir

--- app/Http/Controllers/Virtualcard/VirtualCardController.php
+++ app/Http/Controllers/Virtualcard/VirtualCardController.php
@@ -642,42 +642,49 @@ class VirtualCardController extends Controller
 
         $val = VCard::whereid(Auth::id())->first() ?? null;
 
-if($val == null) {
-
-    $data =  "No Transaction found";
-}
+        if($val != null) {
 
 
+            $curl = curl_init();
+            curl_setopt_array($curl, array(
+                CURLOPT_URL => "https://issuecards.api.bridgecard.co/v1/issuing/cards/get_card_transactions?card_id=$val->card_id",
+                CURLOPT_RETURNTRANSFER => true,
+                CURLOPT_ENCODING => "",
+                CURLOPT_MAXREDIRS => 10,
+                CURLOPT_TIMEOUT => 0,
+                CURLOPT_FOLLOWLOCATION => true,
+                CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+                CURLOPT_CUSTOMREQUEST => "GET",
+                CURLOPT_HTTPHEADER => array(
+                    "Content-Type: application/json",
+                    "token: Bearer " . env('BKEY')
+                ),
+            ));
 
-        $curl = curl_init();
-        curl_setopt_array($curl, array(
-            CURLOPT_URL => "https://issuecards.api.bridgecard.co/v1/issuing/cards/get_card_transactions?card_id=$val->card_id",
-            CURLOPT_RETURNTRANSFER => true,
-            CURLOPT_ENCODING => "",
-            CURLOPT_MAXREDIRS => 10,
-            CURLOPT_TIMEOUT => 0,
-            CURLOPT_FOLLOWLOCATION => true,
-            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-            CURLOPT_CUSTOMREQUEST => "GET",
-            CURLOPT_HTTPHEADER => array(
-                "Content-Type: application/json",
-                "token: Bearer " . env('BKEY')
-            ),
-        ));
+            $var = curl_exec($curl);
+            curl_close($curl);
+            $var = json_decode($var);
+            $card_data = $var->data->transactions ?? null;
 
-        $var = curl_exec($curl);
-        curl_close($curl);
-        $var = json_decode($var);
-        $card_data = $var->data->transactions ?? null;
 
-        if($card_data == null){
+            if($card_data == null){
+                $data =  "No Transaction found";
 
-            $data =  "No Transaction found";
+            }else{
+                $data = $card_data;
+            }
+
+
 
         }else{
 
-            $data = $card_data;
+            $data =  "No Transaction found";
+
         }
+
+
+
+
 
 
 
