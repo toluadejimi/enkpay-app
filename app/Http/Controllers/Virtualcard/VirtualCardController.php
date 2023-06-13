9b25fc1c2a69e3aa0b7036617ad33c6c4bcb7e75
Jimmy <toluadejimi@gmail.com>
2023-06-13 09:30:18 +0100

    gghhhj

--- app/Http/Controllers/Virtualcard/VirtualCardController.php
+++ app/Http/Controllers/Virtualcard/VirtualCardController.php
@@ -127,16 +127,12 @@ class VirtualCardController extends Controller
             ),
         ));
 
-        $var = curl_exec($curl);
-
-        dd($var);
-
-
+        $result = curl_exec($curl);
         curl_close($curl);
-        $var = json_decode($var);
+        $var = json_decode($result);
 
 
-        $error = $var->message ?? "We can not verify your info at the moment";
+        $error = $var->message ?? $result ?? null;
         $status = $var->status ?? null;
 
 
