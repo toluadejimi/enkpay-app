be8f552bc6a3788f8cd0338dba799f083776e90a
Jimmy <toluadejimi@gmail.com>
2023-12-26 21:58:23 +0100

    pushhh

--- app/Http/Controllers/Virtual/VirtualaccountController.php
+++ app/Http/Controllers/Virtual/VirtualaccountController.php
@@ -605,41 +605,41 @@ class VirtualaccountController extends Controller
         }
     }
 
-    public function virtual_acct_history(Request $request)
-    {
+    // public function virtual_acct_history(Request $request)
+    // {
 
-        try {
+    //     try {
 
-            $errand_key = errand_api_key();
+    //         $errand_key = errand_api_key();
 
-            $b_code = env('BCODE');
+    //         $b_code = env('BCODE');
 
-            $acct_no = $request->acct_no;
+    //         $acct_no = $request->acct_no;
 
-            $curl = curl_init();
+    //         $curl = curl_init();
 
-            curl_setopt_array($curl, array(
-                CURLOPT_URL => "https://api.errandpay.com/epagentservice/api/v1/GetSubAccountHistory?businessCode=$b_code&accountNumber=$acct_no&pageNumber=1&pageSize=50",
-                CURLOPT_RETURNTRANSFER => true,
-                CURLOPT_ENCODING => '',
-                CURLOPT_MAXREDIRS => 10,
-                CURLOPT_TIMEOUT => 0,
-                CURLOPT_FOLLOWLOCATION => true,
-                CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-                CURLOPT_CUSTOMREQUEST => 'GET',
-                CURLOPT_HTTPHEADER => array(
-                    "Authorization: Bearer $errand_key",
-                ),
-            ));
+    //         curl_setopt_array($curl, array(
+    //             CURLOPT_URL => "https://api.errandpay.com/epagentservice/api/v1/GetSubAccountHistory?businessCode=$b_code&accountNumber=$acct_no&pageNumber=1&pageSize=50",
+    //             CURLOPT_RETURNTRANSFER => true,
+    //             CURLOPT_ENCODING => '',
+    //             CURLOPT_MAXREDIRS => 10,
+    //             CURLOPT_TIMEOUT => 0,
+    //             CURLOPT_FOLLOWLOCATION => true,
+    //             CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+    //             CURLOPT_CUSTOMREQUEST => 'GET',
+    //             CURLOPT_HTTPHEADER => array(
+    //                 "Authorization: Bearer $errand_key",
+    //             ),
+    //         ));
 
-            $var = curl_exec($curl);
+    //         $var = curl_exec($curl);
 
-            curl_close($curl);
-            $var = json_decode($var);
-        } catch (\Exception $th) {
-            return $th->getMessage();
-        }
-    }
+    //         curl_close($curl);
+    //         $var = json_decode($var);
+    //     } catch (\Exception $th) {
+    //         return $th->getMessage();
+    //     }
+    // }
 
     //PROVIDUS VIRTUAL ACCOUNT
 
