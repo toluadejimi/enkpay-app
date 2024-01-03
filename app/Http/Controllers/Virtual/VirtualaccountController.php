7824ec8dba0ee4913a05c22ef16891d9a8830398
Jimmy <toluadejimi@gmail.com>
2024-01-03 08:12:49 +0100

    pushhh

--- app/Http/Controllers/Virtual/VirtualaccountController.php
+++ app/Http/Controllers/Virtual/VirtualaccountController.php
@@ -877,30 +877,30 @@ class VirtualaccountController extends Controller
                 }
 
 
-                $ck = Transaction::where('receiver_account_no' == $accountNumber)->where('status', 9)->first() ?? null;
-                if($ck != null){
-                    Transaction::where('receiver_account_no' == $accountNumber)
-                    ->where('status', 9)
-                    ->update([
-                        'credit' => $amt_to_credit,
-                        'note' => "$from | Web Pay",
-                        'e_charges' => $deposit_charges,
-                        'enkPay_Cashout_profit' => $enkpay_commision_amount,
-                        'trx_date' => $tranDateTime,
-                        'p_sessionId' => $session_id,
-                        'trx_time' => $tranDateTime,
-                        'sender_name' => $from,
-                        'sender_bank' => $sourceBankName,
-                        'serial_no' => $SerialNumber,
-                        'sender_account_no' => $sourceAccountNumber,
-                        'receiver_account_no' => $accountNumber,
-                        'balance' => $balance,
-                        'status' => 1
-                    ]);
+                // $ck = Transaction::where('receiver_account_no' == $accountNumber)->where('status', 9)->first() ?? null;
+                // if($ck != null){
+                //     Transaction::where('receiver_account_no' == $accountNumber)
+                //     ->where('status', 9)
+                //     ->update([
+                //         'credit' => $amt_to_credit,
+                //         'note' => "$from | Web Pay",
+                //         'e_charges' => $deposit_charges,
+                //         'enkPay_Cashout_profit' => $enkpay_commision_amount,
+                //         'trx_date' => $tranDateTime,
+                //         'p_sessionId' => $session_id,
+                //         'trx_time' => $tranDateTime,
+                //         'sender_name' => $from,
+                //         'sender_bank' => $sourceBankName,
+                //         'serial_no' => $SerialNumber,
+                //         'sender_account_no' => $sourceAccountNumber,
+                //         'receiver_account_no' => $accountNumber,
+                //         'balance' => $balance,
+                //         'status' => 1
+                //     ]);
     
-                    VirtualAccount::where('v_account_no', $accountNumber)->where('state', 1)->update(['state' => 0]);
+                //     VirtualAccount::where('v_account_no', $accountNumber)->where('state', 1)->update(['state' => 0]);
 
-                }
+                // }
 
 
                 //update Transactions
