e8a991b8da98d9b408c4167a8e12db8e0bbadb10
Jimmy <toluadejimi@gmail.com>
2024-07-23 05:43:19 +0100

    beneficary added

--- app/Helpers/User.php
+++ app/Helpers/User.php
@@ -242,37 +242,36 @@ if (!function_exists('send_error')) {
 }
 
 
-if (!function_exists('send_notification')) {
+function send_notification($message)
+{
 
-    function send_notification($message)
-    {
+    $boturl = env('BOTURL');
+    $chat_id = env('BOTID');
 
-        $curl = curl_init();
+    $curl = curl_init();
 
-        curl_setopt_array($curl, array(
-            CURLOPT_URL => 'https://api.telegram.org/bot6140179825:AAGfAmHK6JQTLegsdpnaklnhBZ4qA1m2c64/sendMessage?chat_id=1316552414',
-            CURLOPT_RETURNTRANSFER => true,
-            CURLOPT_ENCODING => '',
-            CURLOPT_MAXREDIRS => 10,
-            CURLOPT_TIMEOUT => 0,
-            CURLOPT_FOLLOWLOCATION => true,
-            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-            CURLOPT_CUSTOMREQUEST => 'POST',
-            CURLOPT_POSTFIELDS => array(
-                'chat_id' => "1316552414",
-                'text' => $message,
+    curl_setopt_array($curl, array(
+        CURLOPT_URL => $boturl,
+        CURLOPT_RETURNTRANSFER => true,
+        CURLOPT_ENCODING => '',
+        CURLOPT_MAXREDIRS => 10,
+        CURLOPT_TIMEOUT => 0,
+        CURLOPT_FOLLOWLOCATION => true,
+        CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+        CURLOPT_CUSTOMREQUEST => 'POST',
+        CURLOPT_POSTFIELDS => array(
+            'chat_id' => $chat_id,
+            'text' => $message,
 
-            ),
-            CURLOPT_HTTPHEADER => array(),
-        ));
+        ),
+        CURLOPT_HTTPHEADER => array(),
+    ));
 
-        $var = curl_exec($curl);
-        curl_close($curl);
+    $var = curl_exec($curl);
+    curl_close($curl);
 
-        $var = json_decode($var);
-    }
+    $var = json_decode($var);
 }
-
 
 if (!function_exists('trx')) {
 
